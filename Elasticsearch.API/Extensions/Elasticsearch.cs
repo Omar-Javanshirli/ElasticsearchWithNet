fbb7dcbadec93d184de1e9267a08eba934b37c32
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-09-13 07:32:30 -0700

    created Product,ProductFeature classes

---
+++ Elasticsearch.API/Extensions/Elasticsearch.cs
@@ -0,0 +1,17 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extensions
+{
+    public static class Elasticsearch
+    {
+        public static void AddElastic(this IServiceCollection services,IConfiguration Configuration)
+        {
+
+            var pool = new SingleNodeConnectionPool(new Uri(Configuration.GetSection("Elastic")["Url"]!));
+            var settings = new ConnectionSettings(pool);
+            var client = new ElasticClient(settings);
+            services.AddSingleton(client);
+        }
+    }
+}
--- Elasticsearch.API/Program.cs
+++ Elasticsearch.API/Program.cs
@@ -1,3 +1,8 @@
+using Elasticsearch.Net;
+using Nest;
+using Elasticsearch.API.Extensions;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddElastic(builder.Configuration);
 
 var app = builder.Build();
 
